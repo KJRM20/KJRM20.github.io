c0115529bdae95a22c179a78bb99dcf78a8cabcb
Karen Rincón <karenrinconjm2016@gmail.com>
2025-04-18 10:39:03 -0500

    feat: agregar cambio de logo por tema

--- scripts/toggleThemes.js
+++ scripts/toggleThemes.js
@@ -1,4 +1,5 @@
 const toggleBtn = document.getElementById('toggle-theme');
+const logoImg = document.getElementById('logo-img');
   const body = document.body;
 
   if (localStorage.getItem('theme') === 'dark') {
@@ -13,4 +14,5 @@ const toggleBtn = document.getElementById('toggle-theme');
     const theme = body.classList.contains('dark-theme') ? 'dark' : 'light';
     localStorage.setItem('theme', theme); 
     toggleBtn.innerHTML = theme == "dark" ? '<i class="fa-solid fa-moon">' : '<i class="fa-solid fa-sun"></i>';
+    logoImg.src = theme == "dark" ? './assets/logo/Logo-white.png' : './assets/logo/Logo.png';
   });
